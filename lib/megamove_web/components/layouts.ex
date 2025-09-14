a89efc35eb505aa7f2f565c2a22a825b27f6fc2e
benoit <benoit@MacStudio-de-benoit.local>
2025-09-14 20:29:04 +0200

    Feat(assets): intégrer les logos MegaMove dans l'application

--- lib/megamove_web/components/layouts.ex
+++ lib/megamove_web/components/layouts.ex
@@ -35,27 +35,34 @@ defmodule MegamoveWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
+    <header class="navbar px-4 sm:px-6 lg:px-8 bg-white shadow-sm">
       <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+        <a href="/" class="flex w-fit items-center gap-3">
+          <img
+            src={~p"/images/logo_Mega_Move.png"}
+            alt="MegaMove Logo"
+            class="h-10 w-auto"
+          />
+          <div class="flex flex-col">
+            <span class="text-lg font-bold text-[#1E3A5F]">MegaMove</span>
+            <span class="text-xs text-[#61A0FF] -mt-1">Transport simplifié</span>
+          </div>
         </a>
       </div>
       <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
+        <ul class="flex flex-row px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
+            <a href="/shipments" class="btn btn-ghost text-[#1E3A5F]">Mes demandes</a>
           </li>
           <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
+            <a href="/carriers" class="btn btn-ghost text-[#1E3A5F]">Transporteurs</a>
           </li>
           <li>
             <.theme_toggle />
           </li>
           <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
+            <a href="/shipments/new" class="btn bg-[#1E3A5F] text-white hover:bg-[#61A0FF]">
+              Créer une demande <span aria-hidden="true">&rarr;</span>
             </a>
           </li>
         </ul>
